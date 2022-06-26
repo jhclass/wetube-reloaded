0c65e5ec943181bebee9a17cb8ecd83901063549
jhclass <jhcode@naver.com>
2022-06-27 01:09:10 +0900

    Router 나열

--- index.js
+++
@@ -1,7 +0,0 @@
-//
-import express from 'express'; //=== const express = require('express');
-
-const app = express();
-console.log('hi');
-
-
--- src/server.js
+++ src/server.js
@@ -3,8 +3,28 @@ import express from "express";
 const app = express();
 
 const port = 4000;
-const eventHandler = ()=>console.log(`Your port Number ${port}`);
 
+const globalRouter = express.Router();
+const handleHome = (req,res)=>res.send("Home");
+globalRouter.get("/",handleHome);
+const userRounter = express.Router();
+const handleEditUser = (req,res)=>res.send("Users!");
+userRounter.get("/edit",handleEditUser);
+const videoRouter = express.Router();
+const handleWatchVideo = (req,res)=>res.send("videos!");
+videoRouter.get("/watch",handleWatchVideo);
+
+app.use("/",globalRouter);
+app.use("/videos",videoRouter);
+app.use("/users",userRounter);
+
+
+
+
+
+
+
+const eventHandler = ()=>console.log(`Your port Number ${port}`);
 app.listen(port,eventHandler);
 
 
