85b649f441ba6a5be2ce4be884bbc49952483a10
jhclass <jhcode@naver.com>
2022-09-13 01:12:01 +0900

    include 하기1

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,23 +1,15 @@
 export const trending = (req,res)=>{
-  res.render('index');
+  res.render('home');
 };
 
-export const see = (req,res) =>{
-  console.log(req.params);
-  return res.send(`현재 보고 있는 비디오는 #${req.params.id}`);
-};
+export const see = (req,res) => res.render('watch');
 
-export const edit = (req,res) => {
-  console.log(req.params);
-  res.send("Edit");
-};
+export const edit = (req,res) => res.render("Edit");
 
 export const search = (req,res) => res.send("Search!");
 
 export const upload = (req,res) => res.send("Upload");
 
-export const deleteVideo = (req,res) => {
-  console.log(req.params);
-    res.send("deleteVideo");
-};
+export const deleteVideo = (req,res) => res.send("deleteVideo");
+
 
