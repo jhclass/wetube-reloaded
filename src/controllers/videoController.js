f16bd116927868dbc945d49454c184befe453645
jhclass <jhcode@naver.com>
2022-09-26 00:19:45 +0900

    mixin

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,6 +1,33 @@
 
 export const trending = (req,res)=>{
-  res.render('home',{pageTitle:"HOME"});
+  const videos = [
+    {
+      title:"video #1",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 minutes ago",
+      views:59,
+      id:1
+    },
+    {
+      title:"video #2",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 minutes ago",
+      views:59,
+      id:2
+    },
+    {
+      title:"video #3",
+      rating: 5,
+      comments: 2,
+      createdAt: "2 minutes ago",
+      views:59,
+      id:3
+    }
+   
+  ];
+  res.render('home',{pageTitle:"HOME",videos});
 };
 
 export const see = (req,res) => res.render('watch');
