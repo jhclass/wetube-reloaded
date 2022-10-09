8dc898043979b7838f5101d3e703c4ad58527301
jhclass <jhcode@naver.com>
2022-10-10 02:25:46 +0900

    비디오 모델 연결

--- src/db.js
+++ src/db.js
@@ -1,6 +1,6 @@
 import mongoose, { mongo } from "mongoose";
 
-mongoose.connect("mongodb://localhost:27017/jintube");
+mongoose.connect("mongodb://127.0.0.1:27017/wetube");
 
 const db = mongoose.connection;
 const handleOpen = ()=>console.log("connected to DB");
--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
 // 
 import "./db";
+import "./models/Video";
 import express from "express";
 import morgan from "morgan";
 import globalRouter from "./routers/globalRouter";
