f38efc01a98a11742db96f14d6c7eb36f0bc5ae3
jhclass <jhcode@naver.com>
2022-11-18 00:44:58 +0900

    mongoose, middleware : pre()

--- src/models/Video.js
+++ src/models/Video.js
@@ -12,7 +12,13 @@ const videoSchema = new mongoose.Schema({
 
 });
 
+
+videoSchema.pre('save',async function(){
+    this.hashtags = this.hashtags[0].split(',').map((word) => word.startsWith("#") ? word : `#${word}` );
+ 
+});
 const Video = mongoose.model("Video",videoSchema);
+
 export default Video;
 
 
