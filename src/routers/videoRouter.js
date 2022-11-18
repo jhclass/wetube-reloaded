134419e64b6cdd17ab5b1f646f657ea52c7b81fc
jhclass <jhcode@naver.com>
2022-11-19 01:45:52 +0900

    mongoose : findByIdAndDelete()

--- src/routers/videoRouter.js
+++ src/routers/videoRouter.js
@@ -6,7 +6,8 @@ const videoRouter = express.Router();
 
 videoRouter.get("/:id([0-9a-f]{24})",watch);
 videoRouter.route("/:id([0-9a-f]{24})/edit").get(getEdit).post(postEdit); //축약
+videoRouter.route("/:id([0-9a-f]{24})/delete").get(deleteVideo)
 videoRouter.route("/upload").get(getUpload).post(postUpload);
-videoRouter.get("/:id(\\d+)/delete",deleteVideo);
+
 
 export default videoRouter;
