c1f13604cea82bdc22593b1b9ad4d11b245fb892
jhclass <jhcode@naver.com>
2022-11-24 00:54:13 +0900

    몽고 DB에 회원데이터 저장

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,10 +1,20 @@
+import User from "../models/User";
 
 export const getJoin = (req,res) => {
     return res.render("join",{pageTitle:"Join"});
 }
-export const postJoin = (req,res) => {
+export const postJoin = async (req,res) => {
    console.log(req.body);
-   res.end();
+   const {name, username, email, password, location} = req.body;
+   await User.create({
+    name, 
+    username, 
+    email, 
+    password, 
+    location
+   });
+   return res.redirect('/login');
+
 }
 
 export const edit = (req,res) => res.send("EditUser");
