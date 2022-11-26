efd3562105b2c6bd18fe2c470d0f60e21c8ba934
jhclass <jhcode@naver.com>
2022-11-26 21:08:29 +0900

    form 에 비밀번호확인하는 input 추가

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -4,8 +4,30 @@ export const getJoin = (req,res) => {
     return res.render("join",{pageTitle:"Join"});
 }
 export const postJoin = async (req,res) => {
-   console.log(req.body);
-   const {name, username, email, password, location} = req.body;
+    console.log(req.body);
+    const {name, username, email, password, password2, location} = req.body;
+    const pageTitle = "join"
+    if(password !== password2) {
+        return res.render("join",{
+            pageTitle,
+            errorMessage:"비밀번호가 서로 일치하지 않습니다."
+        });
+    }
+
+   const usernameExists = await User.exists({username});
+   if (usernameExists){
+    return res.render("join",{
+        pageTitle,
+        errorMessage:"지금 사용중인 아이디는 사용할 수 없습니다."
+    }); 
+   }
+   const useremailExists = await User.exists({email});
+   if (useremailExists){
+    return res.render("join",{
+        pageTitle,
+        errorMessage:"지금 사용중인 이메일은 사용할 수 없습니다."
+    }); 
+   }
    await User.create({
     name, 
     username, 
