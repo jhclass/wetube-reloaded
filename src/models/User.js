557bb956a12a69d2b0501ff2b089ba45e3b3e600
jhclass <jhcode@naver.com>
2022-11-26 21:07:50 +0900

    unipue  key 에는 저장이 안되고 user 에게 에러메세지를 보냄

--- src/models/User.js
+++ src/models/User.js
@@ -1,6 +1,7 @@
 import bcrypt from "bcrypt";
 import mongoose from "mongoose";
-
+//여러개의 조건을 한거번에 사용할꺼면  $or 을 사용하는것도 괜찮다.
+//하지만 지금 각각의 unipue 를 검증하고 에러메세지를 보낼 예정.
 const userSchema = new mongoose.Schema({
     email:{type:String, required:true, unique:true},
     username:{type:String, required:true, unique:true},
