d3890c37d95eb5be815c6d901e1f44d332b2b9bb
jhclass <jhcode0@naver.com>
2022-11-28 01:05:47 +0900

    session part1 : express-session

--- src/server.js
+++ src/server.js
@@ -1,9 +1,11 @@
 // 
 import express from "express";
 import morgan from "morgan";
+import session from "express-session";
 import rootRouter from "./routers/rootRouter";
 import videoRouter from "./routers/videoRouter";
 import userRounter from "./routers/userRouter";
+
 
 
 const app = express();
@@ -13,6 +15,18 @@ app.set('view engine','pug');
 app.set('views', process.cwd()+"/src/views");
 app.use(logger);
 app.use(express.urlencoded({extended:true}));
+app.use(
+    session({
+        secret:"hello!",
+        resave:true,
+        saveUninitialized:true,
+    })
+);
+//쿠키를 제대로 요구req 하는지
+// app.use((req,res,next)=>{
+//     console.log(req.headers);
+//     next();
+// });
 app.use("/",rootRouter);
 app.use("/videos",videoRouter);
 app.use("/users",userRounter);
