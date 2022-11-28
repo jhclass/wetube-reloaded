014ba14fbeb1bc41913799db69476dfb7a1dfe43
jhclass <jhcode0@naver.com>
2022-11-29 01:42:26 +0900

    sessions 과 cookies

--- src/server.js
+++ src/server.js
@@ -15,13 +15,26 @@ app.set('view engine','pug');
 app.set('views', process.cwd()+"/src/views");
 app.use(logger);
 app.use(express.urlencoded({extended:true}));
-app.use(
+app.use( //세션선언
     session({
         secret:"hello!",
         resave:true,
         saveUninitialized:true,
     })
 );
+app.use((req,res,next)=>{ //세션스토어에 담겨있는 내용을 console.log
+    req.sessionStore.all((error,sessions)=>{
+        console.log(sessions);
+        
+    });
+    next();
+});
+app.get("/add-one",(req,res,next)=>{ 
+    // add-one 페이지에 세션아이디와 세션안에 생성한 포테이토에 +=1 하여 브라우저마다 세션이 다르게 적용되고 있다는 것을 확인
+    req.session.potato += 1;
+    return res.send(`${req.session.id}\n${req.session.potato}`);
+});
+
 //쿠키를 제대로 요구req 하는지
 // app.use((req,res,next)=>{
 //     console.log(req.headers);
